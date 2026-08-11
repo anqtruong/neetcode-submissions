5a160903bd375a9bc84f223978a3fdcdf4bd5398
anqtruong <anqtruong77@gmail.com>
2026-08-11 17:11:13 -0500

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,16 @@
+
+class Solution {
+
+
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> mySet = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (mySet.contains(nums[i]))
+                return true;
+            mySet.add(nums[i]);
+        }
+
+        return false;
+    }
+}
